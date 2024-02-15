5f25fb510b18a8175570a5d89566f59c88180031
Bohdan Chupika <boroda4436@users.noreply.github.com>
2024-02-15 17:01:55 +0200

    added detekt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.8.0"
     application
+    id("io.gitlab.arturbosch.detekt") version "1.23.3"
 }
 
 group = "mate.academy"
@@ -22,6 +23,8 @@ kotlin {
     jvmToolchain(11)
 }
 
-application {
-    mainClass.set("MainKt")
+detekt {
+    buildUponDefaultConfig = true // preconfigure defaults
+    allRules = false // activate all available (even unstable) rules.
+    baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
 }
